cc02e252a791647cb83e967258744762f72c0f86
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-12-30 11:22:05 +0530

    Remove build files

--- rust/target/x86_64-unknown-rustos-gnu/release/deps/rustos-399288f8d46ce7fc.d
+++
@@ -1,5 +0,0 @@
-/home/darkaether0x1/github/basic-rustos-grub/rust/target/x86_64-unknown-rustos-gnu/release/deps/librustos-399288f8d46ce7fc.a: src/lib.rs
-
-/home/darkaether0x1/github/basic-rustos-grub/rust/target/x86_64-unknown-rustos-gnu/release/deps/rustos-399288f8d46ce7fc.d: src/lib.rs
-
-src/lib.rs:
--- rust/target/x86_64-unknown-rustos-gnu/release/librustos.d
+++
@@ -1,1 +0,0 @@
-/home/darkaether0x1/github/basic-rustos-grub/rust/target/x86_64-unknown-rustos-gnu/release/librustos.a: /home/darkaether0x1/github/basic-rustos-grub/rust/src/lib.rs
